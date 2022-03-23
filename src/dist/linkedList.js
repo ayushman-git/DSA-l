1c2cc774b93a5b0ea82a6b2cd8578f4a3b59e439
Ayushman Gupta <ayushman1998.1998@gmail.com>
2022-03-23 19:22:23 +0530

    added ts, added intro for linked list

---
+++ src/dist/linkedList.js
@@ -0,0 +1,8 @@
+"use strict";
+// LINKED LIST
+// A linked list is linear collection of nodes which has two parts - data and reference to the next node.
+// Together they form a sequence of nodes
+// DRAWBACKS
+// A drawback of linked lists is that access time is linear (and difficult to pipeline). 
+// Faster access, such as random access, is not feasible. 
+// Arrays have better cache locality as compared to linked lists.
