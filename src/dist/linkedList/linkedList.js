442caec1844d65766a4d760965d9d00c7ac45567
Ayushman Gupta <ayushman1998.1998@gmail.com>
2022-03-23 20:49:43 +0530

    created node and linked list

--- src/dist/linkedList/linkedList.js
+++ src/dist/linkedList/linkedList.js
@@ -5,3 +5,13 @@ class LinkNode {
         this.next = null;
     }
 }
+class LinkedList {
+    constructor(head = null) {
+        this.head = head;
+    }
+}
+const node1 = new LinkNode(23);
+const node2 = new LinkNode(43);
+node1.next = node2;
+const linkedList = new LinkedList(node1);
+console.log(linkedList);
--- src/linkedList/linkedList.ts
+++ src/linkedList/linkedList.ts
@@ -6,3 +6,18 @@ class LinkNode {
     this.next = null;
   }
 }
+
+class LinkedList {
+  head: any;
+  constructor(head: any | undefined = null) {
+    this.head = head;
+  }
+}
+
+const node1 = new LinkNode(23);
+const node2 = new LinkNode(43);
+node1.next = node2;
+
+const linkedList = new LinkedList(node1)
+
+console.log(linkedList)
